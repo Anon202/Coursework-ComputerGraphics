4fe2e16f53b8837349a4551898220b71116ec6ad
Wall <40182161@live.napier.ac.uk>
2016-01-20 17:51:25 +0000

    Working cross product!

--- Ongoing_Assessment/OngoingAssess/vector3/Source.cpp
+++ Ongoing_Assessment/OngoingAssess/vector3/Source.cpp
@@ -7,11 +7,31 @@ int main()
 	int enter; // stops from closing debug
 
 	Vector3 a;
-	Vector3 b(1.0f, 1.0f, 1.0f);
+	Vector3 b(1.0f, 10.0f, 1.0f);
+
+	// need to ask about RHS?
 
 	Vector3 c = a + b;
 
-	std::cout << "Vector c " << std::endl;
+	std::cout << "Vector a " << a.x << ", " << a.y << ", " << a.z << std::endl;
+
+	std::cout << "Vector b " << b.x << ", " << b.y << ", " << b.z << std::endl;
+
+	std::cout << "Vector c " << c.x << ", " << c.y << ", " << c.z << std::endl;
+
+
+
+	Vector3 r(2.0f, -7.0f, 1.0f);
+	Vector3 p(-3.0f, 0.0f, 4.0f);
+
+	Vector3 v;
+	v = v.Cross(p, r);
+
+	std::cout << "Vector v " << v.x << ", " << v.y << ", " << v.z << std::endl;  // answer should be 28, 11, 21
+
+
+
+
 
 	std::cin >> enter;
 
--- Ongoing_Assessment/OngoingAssess/vector3/Vector3.cpp
+++ Ongoing_Assessment/OngoingAssess/vector3/Vector3.cpp
@@ -1,12 +1,76 @@
 #include "vector3.h"
+#include <math.h>
 
-Vector3::Vector3()
+Vector3::Vector3() : x(0), y(0), z(0)
+{}
+
+Vector3::Vector3(float x, float y, float z) : x(x), y(y), z(z)
+{}
+
+Vector3::Vector3(const Vector3& rhs)						/// **************
+{}
+
+void Vector3::operator+= (const Vector3 &v)
 {
-
+	Vector3(x + v.x, y + v.y, z + v.z);
 }
 
-Vector3::Vector3(float x, float y, float z)
-{}
+void Vector3::operator-= (const Vector3 &v)
+{
+	Vector3(x - v.x, y - v.y, z - v.z);
+}
 
-Vector3::Vector3(const Vector3& rhs)
-{}
+void Vector3::operator*= (const float s)
+{
+	Vector3(x*s, y*s, z*s);
+}
+
+Vector3 Vector3::operator/ (const float s) const
+{
+	return Vector3(x/s, y/s, z/s);						// divide components by s
+}
+
+Vector3 Vector3::operator- (const Vector3 &v) const     // subtract components
+{
+	return Vector3(x - v.x, y - v.y, z - v.z);
+}
+
+Vector3 Vector3::operator+  (const Vector3 &v) const
+{
+	return Vector3(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3 Vector3::operator*  (const float s) const
+{
+	return Vector3(x*s, y*s, z*s);
+}
+
+Vector3 Vector3::operator- () const   // UNARY MINUS *******************************
+{
+	return Vector3();
+}
+
+Vector3 Vector3::Cross(const Vector3 &vA, const Vector3& vB)
+{
+	return Vector3((vA.y*vB.z) - (vB.y*vA.z), -((vA.x*vB.z)-(vB.x*vA.z)), (vA.x*vB.y)-(vB.x*vA.y));
+}
+
+float Vector3::Dot(const Vector3 &vA, const Vector3 &vB)
+{
+	return (vA.x*vB.x) + (vA.y*vB.y) + (vA.z*vB.z);
+}
+
+Vector3 Vector3::Normalize(const Vector3 &v)
+{
+	return v / Length(v);
+}
+
+float Vector3::Length(const Vector3 &v)
+{
+	return sqrt(LengthSq(v));
+}
+
+float Vector3::LengthSq(const Vector3 &v)
+{
+	return v.x*v.x + v.y*v.y + v.z*v.z;
+}
