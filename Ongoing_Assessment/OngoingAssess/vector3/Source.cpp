eab03d64a2171105edf0da2d42638715c196fdc9
Wall <40182161@live.napier.ac.uk>
2016-01-20 12:59:19 +0000

    Ongoing Assessment started

---
+++ Ongoing_Assessment/OngoingAssess/vector3/Source.cpp
@@ -0,0 +1,7 @@
+
+#include "vector3.h"
+
+void main()
+{
+	Vector3 a;
+}
---
+++ Ongoing_Assessment/OngoingAssess/vector3/Vector3.cpp
@@ -0,0 +1,12 @@
+#include "vector3.h"
+
+Vector3::Vector3()
+{
+
+}
+
+Vector3::Vector3(float x, float y, float z)
+{}
+
+Vector3::Vector3(const Vector3& rhs)
+{}
