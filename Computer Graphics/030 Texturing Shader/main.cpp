1046887ea5e229520a5214056303e9aec3a14d37
Zoe Wall <zoeoeh@gmail.com>
2016-01-27 13:26:54 +0000

    Building up textures/ multiple objects.

--- Computer Graphics/030 Texturing Shader/main.cpp
+++ Computer Graphics/030 Texturing Shader/main.cpp
@@ -7,56 +7,114 @@ using namespace graphics_framework;
 using namespace glm;
 
 mesh m;
+mesh m2;
 effect eff;
 target_camera cam;
 texture tex;
+texture tex2;
+
+
+float theta = 0.0f;
+float rho = 0.0f;
 
 bool load_content()
 {
 	// Construct geometry object
 	geometry geom;
+	geometry geom2;
+
+	geom.set_type(GL_QUADS);
+	geom2.set_type(GL_QUADS);
+	
 	// Create triangle data
 	// Positions
 	vector<vec3> positions
 	{
-		vec3(0.0f, 1.0f, 0.0f),
+		vec3(-1.0f, 1.0f, 0.0f),
 		vec3(-1.0f, -1.0f, 0.0f),
-		vec3(1.0f, -1.0f, 0.0f)
+		vec3(1.0f, -1.0f, 0.0f),
+		vec3(1.0f, 1.0f, 0.0f),
+		vec3(1.0f, 1.0f, 0.0f),
+		vec3(1.0f, -1.0f, 0.0f),
+		vec3(1.0f, -1.0f, -2.0f),
+		vec3(1.0f, 1.0f, -2.0f)
+
+		
+		
 	};
+
+	vector<vec3> pos2
+	{
+		vec3(-1.0f, 1.0f, -2.0f),
+		vec3(-1.0f, 1.0f, 0.0f),
+		vec3(1.0f, 1.0f, 0.0f),
+		vec3(1.0f, 1.0f, -2.0f)
+	};
+
+	/* 
+		vec3(-1.0f, 1.0f, -2.0f);
+		vec3(-1.0f, -1.0f, 0.0f);
+		vec3(1.0f, 1.0f, 0.0f);
+		vec3(1.0f, 1.0f, -2.0f);
+	*/
 
 	// ***************************************
 	// Define texture coordinates for triangle
 	// ***************************************
 	vector<vec2> tex_coords
 	{
+		vec2(0.0f, 1.0f),
+		vec2(0.0f, 0.0f),
+		vec2(0.5f, 0.0f),
+		vec2(0.5f, 1.0f),
+		vec2(0.5f, 1.0f),
+		vec2(0.5f, 0.0f),
+		vec2(1.0f, 0.0f),
+		vec2(1.0f, 1.0f)
+
+		
+	};
+
+	vector<vec2> tex_coordsTOP
+	{
+		vec2(0.0f, 1.0f),
+		vec2(0.0f, 0.0f),
+		vec2(1.0f, 0.0f),
+		vec2(1.0f, 1.0f),
 
 	};
 
+
 	// Add to the geometry
 	geom.add_buffer(positions, BUFFER_INDEXES::POSITION_BUFFER);
+	geom2.add_buffer(pos2, BUFFER_INDEXES::POSITION_BUFFER);
+
 	// *****************************************
 	// Add texture coordinate buffer to geometry
 	// *****************************************
-	
+	geom.add_buffer(tex_coords, BUFFER_INDEXES::TEXTURE_COORDS_0);
+	geom2.add_buffer(tex_coordsTOP, BUFFER_INDEXES::TEXTURE_COORDS_0);
 
 	// Create mesh object
 	m = mesh(geom);
+	m2 = mesh(geom2);
 
 	// ****************************
 	// Load in texture shaders here
 	// ****************************
-
+	eff.add_shader("..\\resources\\shaders\\simple_texture.vert", GL_VERTEX_SHADER);
+	eff.add_shader("..\\resources\\shaders\\simple_texture.frag", GL_FRAGMENT_SHADER);
 
 	// ************
 	// Build effect
 	// ************
-	
+	eff.build();
 
 	// *********************
 	// Load texture sign.jpg
 	// *********************
-	
-
+	tex = texture("..\\resources\\textures\\sign.jpg");
+	tex2 = texture("..\\resources\\textures\\wood.dds");
 
 	// Set camera properties
 	cam.set_position(vec3(10.0f, 10.0f, 10.0f));
@@ -69,6 +127,15 @@ bool load_content()
 
 bool update(float delta_time)
 {
+
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_UP))
+		theta -= pi<float>() * delta_time;
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_DOWN))
+		theta += pi<float>() * delta_time;
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_RIGHT))
+		rho -= pi<float>() * delta_time;
+	if (glfwGetKey(renderer::get_window(), GLFW_KEY_LEFT))
+		rho += pi<float>() * delta_time;
 	// Update the camera
 	cam.update(delta_time);
 	return true;
@@ -76,13 +143,15 @@ bool update(float delta_time)
 
 bool render()
 {
-	// Bind effect
-	renderer::bind(eff);
-	// Create MVP matrix
-	auto M = m.get_transform().get_transform_matrix();
+	mat4 M = eulerAngleXZ(theta, rho);
 	auto V = cam.get_view();
 	auto P = cam.get_projection();
 	auto MVP = P * V * M;
+
+
+	renderer::bind(eff);
+	renderer::bind(tex, 0);
+
 	// Set MVP matrix uniform
 	glUniformMatrix4fv(
 		eff.get_uniform_location("MVP"), // Location of uniform
@@ -90,19 +159,48 @@ bool render()
 		GL_FALSE, // Transpose the matrix?
 		value_ptr(MVP)); // Pointer to matrix data
 
+
+
+	glUniform1i(eff.get_uniform_location("tex"), 0);
+
+	renderer::render(m);
+
+
+
+#if 1
+	renderer::bind(eff);
+	renderer::bind(tex2, 1);
+	glUniformMatrix4fv(
+		eff.get_uniform_location("MVP"), // Location of uniform
+		1, // Number of values - 1 mat4
+		GL_FALSE, // Transpose the matrix?
+		value_ptr(MVP)); // Pointer to matrix data
+
+	glUniform1i(eff.get_uniform_location("tex"), 1);
+
+	renderer::render(m2);
+#endif
+	// Bind effect
+	
+	// Create MVP matrix
+	//auto M = m.get_transform().get_transform_matrix();
+
+	//m.get_transform().rotate(angleAxis(theta, vec3(1, 0, 0)));
+	//m.get_transform().rotate(angleAxis(rho, vec3(0, 1, 0)));
+
 	// ************************
 	// Bind texture to renderer
 	// ************************
-	
+	//renderer::bind(tex, 0);
+	//renderer::bind(tex2, 1);
 
 	// *****************************************
 	// Set the texture value for the shader here
 	// *****************************************
-	
-
+	//glUniform1i(eff.get_uniform_location("tex"), 0);
 
 	// Render the mesh
-	renderer::render(m);
+
 
 	return true;
 }
--- Computer Graphics/resources/shaders/simple_texture.frag
+++ Computer Graphics/resources/shaders/simple_texture.frag
@@ -2,6 +2,7 @@
 
 // Sampler used to get texture colour
 uniform sampler2D tex;
+//uniform sampler2D tex2;
 
 // Incoming texture coordinate
 layout (location = 0) in vec2 tex_coord;
@@ -13,5 +14,9 @@ void main()
 	// ****************************************
 	// Set out colour to sampled texture colour
 	// ****************************************
+
+	vec4 colour = texture2D(tex, tex_coord);
 	
+	
+	out_colour = colour;
 }
