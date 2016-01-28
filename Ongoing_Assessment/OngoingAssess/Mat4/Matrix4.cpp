0b9ae762833e4d2657128d4b8ed01a06641c60a8
Zoe Wall <zoeoeh@gmail.com>
2016-01-28 21:25:18 +0000

    Matrix Multiplication added. Need to check rotation and transform direction

--- Ongoing_Assessment/OngoingAssess/Mat4/Matrix4.cpp
+++ Ongoing_Assessment/OngoingAssess/Mat4/Matrix4.cpp
@@ -1,19 +1,17 @@
 /* 
  *	Created by Zoe Wall
- *	Last modified 27/01/2016
+ *	Last modified 28/01/2016
  *	Matrix4.cpp - Implementation of the matrix4 header file */
 
 #include "matrix4.h"
+#include <math.h>
 
 Matrix4::Matrix4() // empty Constructor. set to zero?
 { 
-	this->m[0] = { 0 };
-	//this->m[1] 
-	/*
 	for (int i = 0; i <= 16; ++i)
 	{
-		this->m[i] = 0;
-	} */
+		this->m[i] = 0.0f;
+	} 
 }
 
 Matrix4::Matrix4(const Matrix4& rhs)  // rhs constructor, 
@@ -30,28 +28,104 @@ Matrix4::Matrix4(float _00, float _10, float _20, float _30,
 				 float _03, float _13, float _23, float _33)
 {
 	this->m[0] = _00;
-	this->m[1] = _01;
-	this->m[2] = _02;
-	this->m[3] = _03;
-	this->m[4] = _10;
+	this->m[1] = _10;
+	this->m[2] = _20;
+	this->m[3] = _30;
+	this->m[4] = _01;
 	this->m[5] = _11;
-	this->m[6] = _12;
-	this->m[7] = _13;
-	this->m[8] = _20;
-	this->m[9] = _21;
+	this->m[6] = _21;
+	this->m[7] = _31;
+	this->m[8] = _02;
+	this->m[9] = _12;
 	this->m[10] = _22;
-	this->m[11] = _23;
-	this->m[12] = _30;
-	this->m[13] = _31;
-	this->m[14] = _32;
+	this->m[11] = _32;
+	this->m[12] = _03;
+	this->m[13] = _13;
+	this->m[14] = _23;
 	this->m[15] = _33;
 
 }
-/*
-Matrix4 Matrix4::operator* (const Matrix4& lhs, const Matrix4& rhs)
+
+Matrix4 operator* (const Matrix4& lhs, const Matrix4& rhs)
 {
-	return Matrix4(); 
-} */
+	float lhsRow1[4] = { lhs[0], lhs[4], lhs[8], lhs[12] };
+	float lhsRow2[4] = { lhs[1], lhs[5], lhs[9], lhs[13] };
+	float lhsRow3[4] = { lhs[2], lhs[6], lhs[10], lhs[14] };
+	float lhsRow4[4] = { lhs[3], lhs[7], lhs[11], lhs[15] };
+
+	float rhsCol1[4] = { rhs[0], rhs[1], rhs[2], rhs[3] };
+	float rhsCol2[4] = { rhs[4], rhs[5], rhs[6], rhs[7] };
+	float rhsCol3[4] = { rhs[8], rhs[9], rhs[10], rhs[11] };
+	float rhsCol4[4] = { rhs[12], rhs[13], rhs[14], rhs[15] };
+
+	float m0 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m0+= lhsRow1[i] * rhsCol1[i];
+
+	float m1 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m1 += lhsRow2[i] * rhsCol1[i];
+
+	float m2 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m2 += lhsRow3[i] * rhsCol1[i];
+
+	float m3 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m3 += lhsRow4[i] * rhsCol1[i];
+
+	float m4 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m4 += lhsRow1[i] * rhsCol2[i];
+
+	float m5 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m5 += lhsRow2[i] * rhsCol2[i];
+
+	float m6 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m6 += lhsRow3[i] * rhsCol2[i];
+	
+	float m7 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m7 += lhsRow4[i] * rhsCol2[i];
+
+	float m8 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m8 += lhsRow1[i] * rhsCol3[i];
+
+	float m9 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m9 += lhsRow2[i] * rhsCol3[i];
+
+	float m10 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m10 += lhsRow3[i] * rhsCol3[i];
+
+	float m11 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m11 += lhsRow4[i] * rhsCol3[i];
+
+	float m12 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m12 += lhsRow1[i] * rhsCol4[i];
+
+	float m13 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m13 += lhsRow2[i] * rhsCol4[i];
+
+	float m14 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m14 += lhsRow3[i] * rhsCol4[i];
+
+	float m15 = 0.0f;
+	for (int i = 0; i < 4; ++i)
+		m15 += lhsRow4[i] * rhsCol4[i];
+
+	Matrix4 result(m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15);
+
+	return result; 
+}
 
 float& Matrix4::operator[] (int index)
 {
@@ -60,7 +134,7 @@ float& Matrix4::operator[] (int index)
 		//std::cout << "Index out of bounds" << std::endl;
 		return m[0];
 	}
-	
+
 	return m[index];
 }
 
@@ -77,32 +151,19 @@ const float& Matrix4::operator[] (int index) const
 
 Matrix4 Matrix4::Zero()
 {
-
-	return Matrix4();
+	return Matrix4();			// zero matrix return empty constructor matrix (all elements set to zero)
 }
 
 Matrix4 Matrix4::Identity()
 {
-	return Matrix4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); // Matrix4(z[0], z[1], z[2], z[3], z[4], z[5], z[6], z[7], z[8], z[9], z[10]);
+	return Matrix4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1); 
 }
 
 Matrix4 Matrix4::Transpose(const Matrix4& mat)
 {
 	Matrix4 T = mat; // diagonal is the same so set equal first.
 
-	T[4] = mat[1];
-	T[8] = mat[2];
-	T[12] = mat[3];
-	T[9] = mat[6];
-	T[13] = mat[7];
-	T[14] = mat[11];
 
-	T[1] = mat[4];
-	T[2] = mat[8];
-	T[3] = mat[12];
-	T[6] = mat[9];
-	T[7] = mat[13];
-	T[11] =  mat[14];
 	  
 	return T;
 }
@@ -111,9 +172,9 @@ Matrix4 Matrix4::SetTranslation(const Vector3& translation)
 {
 	Matrix4 T = Matrix4::Identity();
 
-	T[3] = translation.x;
-	T[7] = translation.y;
-	T[11] = translation.z;
+	T[12] = translation.x;
+	T[13] = translation.y;
+	T[14] = translation.z;
 
 	return Matrix4(T);
 }
@@ -121,7 +182,8 @@ Matrix4 Matrix4::SetTranslation(const Vector3& translation)
 
 Vector3 Matrix4::GetTranslation(const Matrix4& mat)
 {
-	return Vector3(mat[3], mat[7], mat[11]);
+
+	return Vector3(mat[12], mat[13], mat[14]);
 }
 
 Matrix4 Matrix4::SetScale(const Vector3& scale)
@@ -137,8 +199,26 @@ Matrix4 Matrix4::SetScale(const Vector3& scale)
 
 
 // ref: https://en.wikipedia.org/wiki/Rotation_matrix#Rotation_matrix_from_axis_and_angle
-static Matrix4 SetRotationAxis(const Vector3& axis, float angle)
+Matrix4 Matrix4::SetRotationAxis(const Vector3& axis, float angle)
 {
+	Vector3 unitAxis = Vector3::Normalize(axis);  // normalise the axis vector
+
+	float cosTheta = cos(angle);
+	float negCos = 1 - cosTheta;
+	float sinTheta = sin(angle);
+
+
+	float m00 = cosTheta + unitAxis.x*unitAxis.x*negCos;
+	float m01 = unitAxis.x*unitAxis.y*negCos - unitAxis.z*sinTheta;
+	float m02 = unitAxis.x*unitAxis.z*negCos + unitAxis.y*sinTheta;
+	float m10 = unitAxis.y*unitAxis.x*negCos + unitAxis.z*sinTheta;
+	float m11 = cosTheta + unitAxis.y*unitAxis.y*negCos;
+	float m12 = unitAxis.y*unitAxis.z*negCos - unitAxis.x*sinTheta;
+	float m20 = unitAxis.z*unitAxis.x*negCos - unitAxis.y*sinTheta;
+	float m21 = unitAxis.z*unitAxis.y*negCos + unitAxis.x*sinTheta;
+	float m22 = cosTheta + unitAxis.z*unitAxis.z*negCos;
+
+	return Matrix4(m00, m10, m20, 0.0f, m01, m11, m21, 0.0f, m02, m12, m22, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f);
 
 }
 
--- Ongoing_Assessment/OngoingAssess/Mat4/Source.cpp
+++ Ongoing_Assessment/OngoingAssess/Mat4/Source.cpp
@@ -5,62 +5,71 @@ int main()
 {
 	int enter = 0;
 
-	Matrix4 a();
+	Matrix4 a;
 	Matrix4 b(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 	Matrix4 c(b);
-
-	b[0] = 10;
 	
-	/*
-	std::cout << "Matrix A: \n"
-		<< a[0] << " " << b[1] << " " << b[2] << " " << b[3] << "\n"
-		<< a[4] << " " << b[5] << " " << b[6] << " " << b[7] << "\n"
-		<< a[8] << " " << b[9] << " " << b[10] << " " << b[11] << "\n"
-		<< a[12] << " " << b[13] << " " << b[14] << " " << b[15] << "\n"
-		<< std::endl; */
+	std::cout << "Matrix A (empty constructor): \n";
+	for (int i = 0; i < 4; ++i)
+	{
+		std::cout << a[i] << " " << a[i+4] << " " << a[i+8] << " " << a[i+12] << "\n";
+	}
+	std::cout << std::endl;
 
+	std::cout << "Matrix B (float constructor - column major): \n";
+	for (int i = 0; i < 4; ++i)
+	{
+		std::cout << b[i] << " " << b[i + 4] << " " << b[i + 8] << " " << b[i + 12] << "\n";
+	}
+	std::cout << std::endl;
 
-	std::cout << "Matrix B: \n"
-		<< b[0] << " " << b[1] << " " << b[2] << " " << b[3] << "\n"
-		<< b[4] << " " << b[5] << " " << b[6] << " " << b[7] << "\n"
-		<< b[8] << " " << b[9] << " " << b[10] << " " << b[11] << "\n"
-		<< b[12] << " " << b[13] << " " << b[14] << " " << b[15] << "\n"
-		<< std::endl;
-
-	std::cout << "Matrix C: \n"
-		<< c[0] << " " << c[1] << " " << c[2] << " " << c[3] << "\n"
-		<< c[4] << " " << c[5] << " " << c[6] << " " << c[7] << "\n"
-		<< c[8] << " " << c[9] << " " << c[10] << " " << c[11] << "\n"
-		<< c[12] << " " <<c[13] << " " << c[14] << " " << c[15] << "\n"
-		<< std::endl;
-
-
+	std::cout << "Matrix C (rhs constructor (passed in b): \n";
+	for (int i = 0; i < 4; ++i)
+	{
+		std::cout << c[i] << " " << c[i + 4] << " " << c[i + 8] << " " << c[i + 12] << "\n";
+	}
+	std::cout << std::endl;
 
 	Vector3 v(10, 5, 9);
 	Matrix4 d = Matrix4::SetTranslation(v);
 
+	std::cout << "Matrix Translation D: \n";
+	for (int i = 0; i < 4; ++i)
+	{
+		std::cout << d[i] << " " << d[i + 4] << " " << d[i + 8] << " " << d[i + 12] << "\n";
+	}
+	std::cout << std::endl;
 
-	std::cout << "Matrix D: Translation Matrix by x = 10, y = 5, z = 9\n"
-		<<d[0] << " " << d[1] << " " << d[2] << " " << d[3] << "\n"
-		<<d[4] << " " << d[5] << " " << d[6] << " " << d[7] << "\n"
-		<<d[8] << " " << d[9] << " " << d[10] << " " <<d[11] << "\n"
-		<<d[12] << " " <<d[13] << " " <<d[14] << " " <<d[15] << "\n"
-		<< std::endl;
 
 	Vector3 tran = Matrix4::GetTranslation(d);
-	std::cout << "Get translation from Translation matrix T: \n"
-		<< tran.x << " " << tran.y << " " << tran.z << std::endl;
+	std::cout << "Get translation from Translation matrix D: \n"
+		<< tran.x << " " << tran.y << " " << tran.z << "\n" << std::endl;
 
 	Vector3 scale(2, 2, 2);
 	Matrix4 e = Matrix4::SetScale(scale);
 
+	std::cout << "Matrix E Scale Matrix (2, 2, 2): \n";
+	for (int i = 0; i < 4; ++i)
+	{
+		std::cout << e[i] << " " << e[i + 4] << " " << e[i + 8] << " " << e[i + 12] << "\n";
+	}
+	std::cout << std::endl;
 
-	std::cout << "Matrix C: \n"
-		<< e[0] << " " << e[1] << " " << e[2] << " " << e[3] << "\n"
-		<< e[4] << " " << e[5] << " " << e[6] << " " << e[7] << "\n"
-		<< e[8] << " " << e[9] << " " << e[10] << " " <<e[11] << "\n"
-		<< e[12] << " " << e[13] << " " << e[14] << " " << e[15] << "\n"
-		<< std::endl;
+	Matrix4 id = Matrix4::Identity();
+	std::cout << "Identity Matrix: \n";
+	for (int i = 0; i < 4; ++i)
+	{
+		std::cout << id[i] << " " << id[i + 4] << " " << id[i + 8] << " " << id[i + 12] << "\n";
+	}
+	std::cout << std::endl;
+
+	Matrix4 zero = Matrix4::Zero();
+	std::cout << "Zero Matrix: \n";
+	for (int i = 0; i < 4; ++i)
+	{
+		std::cout << zero[i] << " " << zero[i + 4] << " " << zero[i + 8] << " " << zero[i + 12] << "\n";
+	}
+	std::cout << std::endl;
 
 	Vector3 transformed_point = Matrix4::TransformPoint(d, v);
 
@@ -68,14 +77,22 @@ int main()
 		<< v.x << " " << v.y << " " << v.z << "\n"
 		<< "After: " << transformed_point.x << " " << transformed_point.y << " " << transformed_point.z << std::endl;
 
-	Vector3 transformed_pt = Matrix4::TransformDirection(d, v);
 
-	std::cout << "Transform point v by Translation matrix.\nBefore: "
+	Vector3 v2(0.0, 1.0, 3.0);
+	Vector3 transformed_pt = Matrix4::TransformDirection(d, v2);
+
+	std::cout << "Transform v by Translation matrix.\nBefore: "
 		<< v.x << " " << v.y << " " << v.z << "\n"
 		<< "After: " << transformed_pt.x << " " << transformed_pt.y << " " << transformed_pt.z << std::endl;
 
-
-
+	Matrix4 matMul = b * c;
+	std::cout << "Matrix multiplication B*C: \n";
+	for (int i = 0; i < 4; ++i)
+	{
+		std::cout << matMul[i] << " " << matMul[i + 4] << " " << matMul[i + 8] << " " << matMul[i + 12] << "\n";
+	}
+	std::cout << std::endl;
+	
 	std::cin >> enter;
 
 	return 0;
